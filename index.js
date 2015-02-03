5e798369ddbcf4ea4283b2aadaba4cec6f47d437
jonschlinkert <jon.schlinkert@sellside.com>
2015-02-02 23:29:25 -0500

    refactor

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
-/*!
- * delimiters <https://github.com/jonschlinkert/delimiters>
- *
- * Copyright (c) 2014 Jon Schlinkert, contributors
- * Licensed under the MIT License (MIT)
- */
-
 'use strict';
 
-var _ = require('lodash');
-var Delims = require('delims');
-var delims = new Delims();
-
+var extend = require('extend-shallow');
+var makeDelims = require('delimiter-regex');
 
 /**
- * ## Delimiters
- *
+ * Expose `Delimiters`
+ */
+
+module.exports = Delimiters;
+
+/**
  * Create a new instance of `Delimiters`, optionally
  * passing default `options` to use.
  *
@@ -30,221 +25,102 @@ var delims = new Delims();
  * @api public
  */
 
-function Delimiters(options) {
-  this.options = options || {};
-  this.delims = this.options.delims || {};
-  this.initDelims();
+function Delimiters() {
+  this.delims = {};
 }
 
-
 /**
- * Initialize default configuration, or
- * initialize with given `options`.
- *
- * @api private
- */
-
-Delimiters.prototype.initDelims = function(options) {
-  var opts = options || this.options;
-
-  this.option('flags', opts.flags || 'g');
-  this.option('layoutDelims', opts.layoutDelims || ['{{', '}}']);
-  this.option('layoutTag', opts.layoutTag || 'body');
-
-  this.addDelims('default', ['<%', '%>']);
-  this.addDelims('es6', ['${', '}'], {
-    interpolate: /\$\{([^\\}]*(?:\\.[^\\}]*)*)\}/g
-  });
-};
-
-
-/**
- * ## .option
- *
- * Set or get an option.
- *
- * ```js
- * delimiters.option('a', true)
- * delimiters.option('a')
- * // => true
- * ```
- *
- * @method option
- * @param {String} `key`
- * @param {*} `value`
- * @return {*}
- * @api public
- */
-
-Delimiters.prototype.option = function(key, value) {
-  var args = [].slice.call(arguments);
-
-  if (args.length === 1 && typeof key === 'string') {
-    return this.options[key];
-  }
-
-  if (typeof key === 'object' && !Array.isArray(key)) {
-    _.extend.apply(_, [this.options].concat(args));
-    return this;
-  }
-
-  this.options[key] = value;
-  return this;
-};
-
-
-/**
- * ## .makeTag
- *
- * Generate the default body tag to use as a
- * fallback, based on the `tag` and `delims`
- * defined in the options.
- *
- * @param  {Object} options
- * @return {String} The actual body tag, e.g. `{{ body }}`
- * @api private
- */
-
-Delimiters.prototype.makeTag = function (options) {
-  var opts = _.extend({}, this.options, options);
-
-  return [
-    opts.delims[0],
-    opts.tag,
-    opts.delims[1]
-  ].join(opts.sep || ' ');
-};
-
-
-/**
- * ## .makeRegex
- *
- * Return a regular expression for the "body" tag based on the
- * `tag` and `delims` defined in the options.
- *
- * @param  {Object} `options`
- * @return {RegExp}
- * @api private
- */
-
-Delimiters.prototype.makeRegex = function (options) {
-  var opts = _.extend({sep: '\\s*'}, this.options, options);
-  var tag = this.makeTag(opts).replace(/[\]()[{|}]/g, '\\$&');
-  return new RegExp(tag, opts.flags);
-};
-
-
-/**
- * ## .makeDelims
- *
  * Pass custom delimiters to Lo-Dash.
  *
  * **Example:**
  *
  * ```js
- * delimiters.makeDelims(['{%', '%}'], ['{{', '}}'], opts);
- * ```
- *
- * @param  {Array} `delims` Array of delimiters.
- * @param  {Array} `layoutDelims` layout-specific delimiters to use. Default is `['{{', '}}']`.
- * @param  {Object} `options` Options to pass to [delims].
- * @api private
- */
-
-Delimiters.prototype.makeDelims = function (delimiters, options) {
-  var settings = _.defaults({escape: true}, options);
-  return _.extend(delims.templates(delimiters, settings), options);
-};
-
-
-/**
- * ## .addDelims
- *
- * Store delimiters by `name` with the given `options` for later use.
- *
- * **Example:**
- *
- * ```js
- * delimiters.addDelims('curly', ['{%', '%}']);
- * delimiters.addDelims('angle', ['<%', '%>']);
- * delimiters.addDelims('es6', ['${', '}'], {
+ * delims.makeDelims(['${', '}'], {
  *   // override the generated regex
  *   interpolate: /\$\{([^\\}]*(?:\\.[^\\}]*)*)\}/g
  * });
  * ```
  *
- * [delims]: https://github.com/jonschlinkert/delims "Generate regex for delimiters"
- *
- * @param {String} `name` The name to use for the stored delimiters.
- * @param {Array} `delims` Array of delimiter strings. See [delims] for details.
- * @param {Object} `opts` Options to pass to [delims]. You can also use the options to
- *                        override any of the generated delimiters.
+ * @param  {Array} `arr` Array of delimiters.
+ * @param  {Object} `settings` Override specific generated delimiters with custom values.
  * @api public
  */
 
-Delimiters.prototype.addDelims = function (name, delimiters, opts) {
-  var delims = this.makeDelims(delimiters, opts);
-  this.delims[name] = _.defaults({}, opts, delims);
+Delimiters.prototype.makeDelims = function(arr, settings) {
+  var res = {original: arr};
+  res.escape = makeDelims(arr[0] + '-', arr[1], settings);
+  res.evaluate = makeDelims(arr[0], arr[1], settings);
+  res.interpolate = makeDelims(arr[0] + '=', arr[1], settings);
+  return res;
+};
+
+/**
+ * Cache delimiters by `ext` with the given `options` for later use.
+ *
+ * **Example:**
+ *
+ * ```js
+ * delims.setDelims('curly', ['{%', '%}']);
+ * delims.setDelims('angle', ['<%', '%>']);
+ * delims.setDelims('es6', ['${', '}'], {
+ *   // override the generated regex
+ *   interpolate: /\$\{([^\\}]*(?:\\.[^\\}]*)*)\}/g
+ * });
+ * ```
+ *
+ * @param {String} `ext` The name to use for the stored delimiters.
+ * @param {Array} `arr` Array of delimiter strings.
+ * @api public
+ */
+
+Delimiters.prototype.setDelims = function(ext, arr, settings) {
+  if (typeof ext !== 'string') {
+    throw new Error('setDelims expects a string for `ext`');
+  }
+
+  var res = {orig: null, interpolate: null, evaluate: null, escape: null};
+  if (Array.isArray(arr)) {
+    res = extend({}, this.makeDelims(arr, settings));
+  } else if (arr instanceof RegExp) {
+    res = extend(res, {interpolate: arr});
+  } else if (typeof arr === 'string') {
+    res = extend(res, {interpolate: new RegExp(arr, 'g')});
+  } else if (typeof arr === 'object') {
+    res = extend(res, arr, settings);
+  }
+
+  this.delims[ext] = res;
   return this;
 };
 
-
 /**
- * ## .getDelims
- *
- * The `name` of the stored delimiters to pass to the current delimiters engine.
+ * The `ext` of the stored delimiters to pass to the current delimiters engine.
  * The engine must support custom delimiters for this to work.
  *
- * @param  {Array} `name` The name of the stored delimiters to pass.
+ * @param  {Array} `ext` The ext of the stored delimiters to pass.
  * @api private
  */
 
-Delimiters.prototype.getDelims = function(name) {
-  if(this.delims.hasOwnProperty(name)) {
-    return this.delims[name];
+Delimiters.prototype.getDelims = function(ext) {
+  if(this.delims.hasOwnProperty(ext)) {
+    return this.delims[ext];
   }
-  name = this.currentDelims || 'default';
-  return this.delims[name];
+  ext = this.currentDelims || 'default';
+  return this.delims[ext];
 };
 
-
 /**
- * ## .useDelims
- *
- * Specify by `name` the delimiters to make active.
+ * Specify by `ext` the delimiters to make active.
  *
  * ```js
  * delimiters.useDelims('curly');
  * delimiters.useDelims('angle');
  * ```
  *
- * @param {String} `name`
+ * @param {String} `ext`
  * @api public
  */
 
-Delimiters.prototype.useDelims = function(name) {
-  return this.currentDelims = name;
+Delimiters.prototype.useDelims = function(ext) {
+  return this.currentDelims = ext;
 };
-
-
-/**
- * ## .assertDelims
- *
- * Return `true` if delimiters are found in the given `str`.
- *
- * @param  {String} `str`
- * @return {Boolean}
- * @api private
- */
-
-Delimiters.prototype.assertDelims = function (str, re) {
-  re = re || this.getDelims();
-
-  return re ? str.indexOf(re.delims[0]) !== -1 &&
-    str.indexOf(re.delims[1]) !== -1 :
-    str.indexOf('<%') !== -1 ||
-    str.indexOf('${') !== -1;
-};
-
-
-module.exports = Delimiters;
